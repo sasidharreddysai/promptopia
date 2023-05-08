c7a7bd318a1320fbd51297269aa1c72ba2c86871
sasidharreddysai <sasidharreddysai@gmail.com>
2023-05-08 22:40:17 +0530

    Create and View Prompts

---
+++ app/profile/page.jsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${sessio?.user.id}/posts`);
+      const data = await response.json();
+
+      setPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = () => {};
+
+  const handleDelete = () => {};
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default MyProfile;
---
+++ components/Profile.jsx
@@ -0,0 +1,14 @@
+import PromptCard from "./PromptCard";
+
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+  return (
+    <section className="w-full">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{name} Profile</span>
+      </h1>
+      <p className="desc text-left">{desc}</p>
+    </section>
+  );
+};
+
+export default Profile;
